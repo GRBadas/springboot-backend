8303b568e6929d8a9859feac88f95e2465586ad9
Gabriel Badaró <grbadas@gmail.com>
2023-02-23 14:50:14 -0300

    update

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -7,12 +8,19 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-        <HeaderComponent />
+    <Router>
       <div className="container">
-        <ListEmployeeComponent />
-      </div>
+        <HeaderComponent />
+        <div className="container">
+            <Routes>
+              <Route path='/' element ={<ListEmployeeComponent />} />
+              <Route path='/employees' element ={<ListEmployeeComponent />} />
+            </Routes> 
+        </div>
         <FooterComponent />
-    </div>
+      </div>
+    </Router>
+  </div>
   );
 }
 
